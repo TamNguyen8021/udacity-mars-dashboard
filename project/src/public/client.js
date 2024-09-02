12262ae779d3d2623dc1dc611a58b1e8c85b441c
TamNguyen8021 <tamntt8021@gmail.com>
2024-09-02 20:25:17 +0700

    feat: create dashboard to view rover photos

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,105 +1,180 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+let store = Immutable.Map({
+	rovers: Immutable.List(["Curiosity", "Opportunity", "Spirit"]),
+	photos: Immutable.List(),
+});
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+const updateStore = (newState) => {
+	store = store.merge(newState);
+	render(root, store);
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
-
+	root.innerHTML = App();
+	showRoverButtons(state.get("rovers"));
+};
 
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
+const App = () => {
+	return `
+		<main>
+				<h1>Welcome to Mars dashboard</h1>
+				<section>
+						<h3>Each rover has its own set of photos. Select one of them to see more details.</h3>
+						<div id="rover-buttons"></div>
+				</section>
+		</main>
+	`;
+};
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
-})
+window.addEventListener("load", () => {
+	render(root, store);
+});
+
+/**
+ * @description Higher-Order Function that adds a loading indicator while the async function is running
+ * @param {Function} asyncMethod The asynchronous function to wrap with a loading indicator
+ * @returns {Function} A new function that shows a loading indicator while the original function is running
+ */
+const showLoader =
+	(asyncMethod) =>
+	async (...args) => {
+		const loadingElement = document.createElement("div");
+		loadingElement.innerHTML = "<p>Loading...</p>";
+		document.getElementById("root").appendChild(loadingElement);
+
+		try {
+			const result = await asyncMethod(...args);
+			loadingElement.remove();
+			return result;
+		} catch (error) {
+			loadingElement.remove();
+			throw error;
+		}
+	};
+
+/**
+ * @description Higher-Order Function that adds error handling to an asynchronous function
+ * @param {Function} asyncMethod The asynchronous function to wrap with error handling
+ * @returns {Function} A new function that catches and logs errors, and returns an error message
+ */
+const showError =
+	(asyncMethod) =>
+	async (...args) => {
+		try {
+			return await asyncMethod(...args);
+		} catch (error) {
+			console.error("An error occurred: ", error);
+			return `<div class="error">An error occurred: ${error.message}</div>`;
+		}
+	};
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
+/**
+ * @description Renders rover's information
+ *
+ * @param {string} name Name of the rover
+ */
+const showRoverInformation = async (name) => {
+	const imageGallery = await ImageGallery(name);
+	const galleryContainer = document.createElement("div");
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
+	galleryContainer.id = "image-gallery-container";
+	galleryContainer.innerHTML = imageGallery;
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+	const root = document.getElementById("root");
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+	root.appendChild(galleryContainer);
+};
 
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
+/**
+ * @description Renders list of buttons to view selected rover's information
+ *
+ * @param {Array<string>} rovers List of rovers' names
+ */
+const showRoverButtons = (rovers) => {
+	const buttonContainer = document.getElementById("rover-buttons");
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
+	rovers.forEach((rover) => {
+		const button = document.createElement("button");
+		button.textContent = rover;
+		button.addEventListener("click", () => showRoverInformation(rover));
+		buttonContainer.appendChild(button);
+	});
+};
+
+/**
+ * @description Renders image gallery of the selected rover
+ *
+ * @param {Array<object>} photos List of the selected rover's photos
+ * @param {string} roverName The selected rover's name
+ */
+const showPhotoGallery = (photos, roverName) => {
+	return photos
+		.map(
+			(photo) =>
+				`
+		<div class="photo-details-container">
+			<p>Date photo were taken: ${photo.earth_date}</p>
+			<img class="photo" alt="Photo taken by ${roverName} rover" src=${photo.img_src} loading="lazy" />
+		</div>
+		`
+		)
+		.join("");
+};
+
+/**
+ * @description Renders image gallery of the selected rover
+ *
+ * @param {string} name Name of the rover
+ */
+const ImageGallery = showLoader(async (name) => {
+	const galleryContainer = document.getElementById("image-gallery-container");
+
+	if (galleryContainer) {
+		// Clear existing image gallery before calling api
+		galleryContainer.innerHTML = "";
+	}
+
+	await getRoverPhotos(name);
+
+	const photos = store.get("photos");
+
+	if (!photos?.size) {
+		return "<p>No photos available for this rover.</p>";
+	}
+
+	const rover = photos.get(0).rover;
+
+	return `
+		<div class="image-gallery">
+			<p>Name: ${name}</p>
+			<p>Launch date: ${rover.launch_date}</p>
+			<p>Landing date: ${rover.landing_date}</p>
+			<p>Status: ${rover.status}</p>
+			<div class="photo-grid">
+				${showPhotoGallery(photos.toJS(), name)}
+			</div>
+		</div>		
+	`;
+});
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
-
-    return data
-}
+/**
+ * @description Gets photos of the selected rover
+ *
+ * @param {string} name Name of the rover
+ * @param {number} sol Martian sol of the rover's mission
+ */
+const getRoverPhotos = showError(async (name, sol = 1000) => {
+	const response = await fetch(
+		`http://localhost:3000/rovers/${name}?sol=${sol}`
+	);
+	const data = await response.json();
+	updateStore({photos: Immutable.List(data.photos)});
+});
